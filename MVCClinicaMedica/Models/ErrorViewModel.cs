dd9fa94a840504faf2b8efc24e42734ff662c3f7
MelvinC1999 <melvingordito1999@gmail.com>
2023-08-09 19:29:14 -0500

    Agregar archivos de proyecto.

---
+++ MVCClinicaMedica/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCClinicaMedica.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
