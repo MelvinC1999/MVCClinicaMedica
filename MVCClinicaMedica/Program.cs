153f63bbe305c6a41b1e11725a89d43c9856c67f
Sebastian Arguello <sebastian.arguello.99@gmail.com>
2023-08-15 18:01:36 -0500

    agregado la linea app.Run() :v y las migraciones

--- MVCClinicaMedica/Program.cs
+++ MVCClinicaMedica/Program.cs
@@ -62,4 +62,5 @@ app.MapControllerRoute(
     name: "default",
     //pattern: "{controller=Inicio}/{action=IniciarSesion}/{id?}");
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.Run();
 
