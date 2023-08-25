b05356d781edd36ff34b9f37bd64ca5f2ee2a849
Sebastian Arguello <sebastian.arguello.99@gmail.com>
2023-08-25 15:14:09 -0500

    ajustes para que valga en mi compu

--- MVCClinicaMedica/Program.cs
+++ MVCClinicaMedica/Program.cs
@@ -67,6 +67,8 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Inicio}/{action=IniciarSesion}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+//pattern: "{controller=Inicio}/{action=IniciarSesion}/{id?}");
+
 
 app.Run();
